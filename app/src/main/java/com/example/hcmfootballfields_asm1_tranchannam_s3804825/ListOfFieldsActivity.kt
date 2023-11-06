2390833d6bae597154dd72f67cd4d29530c988a3
Nick-Tran1501 <channam1501@gmail.com>
2023-11-06 14:40:53 +0700

    Create and Setup Theme

---
+++ app/src/main/java/com/example/hcmfootballfields_asm1_tranchannam_s3804825/ListOfFieldsActivity.kt
@@ -0,0 +1,27 @@
+package com.example.hcmfootballfields_asm1_tranchannam_s3804825
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.compose.AppTheme
+
+class ListOfFieldsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Nick")
+                }
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "HCMFootballFields_ASM1_TranChanNam_s3804825"
+include(":app")
