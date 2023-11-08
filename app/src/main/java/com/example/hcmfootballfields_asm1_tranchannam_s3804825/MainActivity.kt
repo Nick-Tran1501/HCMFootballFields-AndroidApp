5af40b78d9e04dd57b24a66090575fbfdf5a756f
Nick-Tran1501 <channam1501@gmail.com>
2023-11-08 16:33:56 +0700

    Login Screen

--- app/src/main/java/com/example/hcmfootballfields_asm1_tranchannam_s3804825/MainActivity.kt
+++ app/src/main/java/com/example/hcmfootballfields_asm1_tranchannam_s3804825/MainActivity.kt
@@ -3,22 +3,32 @@ package com.example.hcmfootballfields_asm1_tranchannam_s3804825
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -51,18 +61,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun FirstScreen(navController: NavController, themeViewModel: ThemeViewModel) {
+    val isDarkTheme = themeViewModel.isDarkTheme.value
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
     ) {
+
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp),
             horizontalArrangement = Arrangement.End
         ) {
-            val isDarkTheme = themeViewModel.isDarkTheme.value
+
             IconButton(onClick = { themeViewModel.toggleTheme() }) {
                 Icon(
                     imageVector = if (isDarkTheme) ImageVector.vectorResource(id = R.drawable.light_mode) else ImageVector.vectorResource(
@@ -75,7 +87,49 @@ fun FirstScreen(navController: NavController, themeViewModel: ThemeViewModel) {
         }
 
     }
-
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .height(100.dp),
+            color = MaterialTheme.colorScheme.primary,
+            text = "Welcome To HCM FootFields",
+            fontSize = 48.sp,
+            lineHeight = 48.sp,
+            textAlign = TextAlign.Center
+        )
+        Image(
+            painter = if (!isDarkTheme) painterResource(id = R.drawable.ballonly) else painterResource(
+                id = R.drawable.ballonlydark
+            ),
+            contentDescription = "Application Icon Theme",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(350.dp)
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .height(100.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            text = "Show Your Talents Anywhere and Anytime",
+            fontSize = 24.sp,
+            lineHeight = 48.sp,
+            textAlign = TextAlign.Center
+        )
+        Button(
+            onClick = { navController.navigate("list_of_fields") },
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text(text = "Let's Start")
+        }
+    }
 }
 
 @Composable
